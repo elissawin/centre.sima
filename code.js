87375b51bb74cdcd7f2bcff99668a909631c3e91
elissawin <113803240+elissawin@users.noreply.github.com>
2022-09-25 23:01:30 +0100

    Add files via upload

---
+++ code.js
@@ -0,0 +1,156 @@
+function show_hide_1() {
+
+        document.getElementById("V1").style.display = "block";
+
+        document.getElementById("V2").style.display = "none";
+
+        document.getElementById("V3").style.display = "none";
+
+    }
+
+function show_hide_2() {
+
+      document.getElementById("V1").style.display = "none";
+
+      document.getElementById("V2").style.display = "block";
+
+      document.getElementById("V3").style.display = "none";
+
+    }
+
+function show_hide_3() {
+
+      document.getElementById("V1").style.display = "none";
+
+      document.getElementById("V2").style.display = "none";
+
+      document.getElementById("V3").style.display = "block";
+   
+    }
+
+//partie 2
+
+function show_hide_4() {
+
+        document.getElementById("B1").style.display = "block";
+
+        document.getElementById("B2").style.display = "none";
+
+        document.getElementById("B3").style.display = "none";
+
+    }
+
+function show_hide_5() {
+
+      document.getElementById("B1").style.display = "none";
+
+      document.getElementById("B2").style.display = "block";
+
+      document.getElementById("B3").style.display = "none";
+
+    }
+
+function show_hide_6() {
+
+      document.getElementById("B1").style.display = "none";
+
+      document.getElementById("B2").style.display = "none";
+
+      document.getElementById("B3").style.display = "block";
+   
+    }
+
+//partie 3
+
+function show_hide_7() {
+
+        document.getElementById("C1").style.display = "block";
+
+        document.getElementById("C2").style.display = "none";
+
+        document.getElementById("C3").style.display = "none";
+
+    }
+
+function show_hide_8() {
+
+      document.getElementById("C1").style.display = "none";
+
+      document.getElementById("C2").style.display = "block";
+
+      document.getElementById("C3").style.display = "none";
+
+    }
+
+function show_hide_9() {
+
+      document.getElementById("C1").style.display = "none";
+
+      document.getElementById("C2").style.display = "none";
+
+      document.getElementById("C3").style.display = "block";
+   
+    }
+
+ //partie 4
+
+function show_hide_10() {
+
+        document.getElementById("D1").style.display = "block";
+
+        document.getElementById("D2").style.display = "none";
+
+        document.getElementById("D3").style.display = "none";
+
+    }
+
+function show_hide_11() {
+
+      document.getElementById("D1").style.display = "none";
+
+      document.getElementById("D2").style.display = "block";
+
+      document.getElementById("D3").style.display = "none";
+
+    }
+
+function show_hide_12() {
+
+      document.getElementById("D1").style.display = "none";
+
+      document.getElementById("D2").style.display = "none";
+
+      document.getElementById("D3").style.display = "block";
+   
+    }
+
+ //partie 5
+
+function show_hide_13() {
+
+        document.getElementById("F1").style.display = "block";
+
+        document.getElementById("F2").style.display = "none";
+
+        document.getElementById("F3").style.display = "none";
+    }
+
+function show_hide_14() {
+
+      document.getElementById("F1").style.display = "none";
+
+      document.getElementById("F2").style.display = "block";
+
+      document.getElementById("F3").style.display = "none";
+
+    }
+
+function show_hide_15() {
+
+      document.getElementById("F1").style.display = "none";
+
+      document.getElementById("F2").style.display = "none";
+
+      document.getElementById("F3").style.display = "block";
+   
+    }
